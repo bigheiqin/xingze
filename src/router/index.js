d1515ea7e7a0ec65f18498c5c6f9f983fc8cb5fd
nong99 <nong99@outlook.com>
2017-07-24 08:43:28 +0800

    up

--- src/router/index.js
+++ src/router/index.js
@@ -1,86 +1,90 @@
+// **这是一条 路由懒加载：结合Vue异步组件及webpack的 code splitting feature
+// **这使得打包的时候每个组件（路由）都生成一个对应的独立的.js文件
+// component (resolve) { require(['../views/user/UserFans'], resolve) }
+// 但是如果把所有文件都合并一起，首屏加载特别慢，所以 在能够异步的则异步
 export default [{
     path: '/',
     name: 'root-1',
-    component: require('../views/Home')
+    component: require('../views/home')
 }, { // 首页
     path: '/home',
     name: 'home',
-    component: require('../views/Home')
+    component: require('../views/home')
 }, { // 用户中心
     path: '/user',
     name: 'user',
-    component: require('../views/User')
+    component: require('../views/user')
 }, { // 我的勋章
     path: '/user/medal',
     name: 'Medal',
-    component: require('../views/Medal')
+    component (resolve) { require(['../views/Medal'], resolve) }
 }, { // 智能设备 Ridingdevice
     path: '/user/device',
     name: '/user/device',
-    component: require('../views/Ridingdevice')
+    component (resolve) { require(['../views/Ridingdevice'], resolve) }
 }, { // 等级权限说明 Rank
     path: '/user/rank',
     name: '/user/rank',
-    component: require('../views/Rank')
+    component (resolve) { require(['../views/Rank'], resolve) }
 }, { // 个人中心后台 userAdmin
     path: '/user/userAdmin',
     name: '/user/userAdmin',
-    component: require('../views/UserAdmin')
+    component (resolve) { require(['../views/UserAdmin'], resolve) }
 }, { // 修改手机号码  phone
     path: '/user/phone',
     name: '/user/phone',
-    component: require('../views/user/PhoneEdit')
+    component (resolve) { require(['../views/user/PhoneEdit'], resolve) }
 }, { // 绑定邮箱  email
     path: '/user/email',
     name: '/user/email',
-    component: require('../views/user/EmailEdit')
+    component (resolve) { require(['../views/user/EmailEdit'], resolve) }
 }, { // 修改密码  password
     path: '/user/password',
     name: '/user/password',
-    component: require('../views/user/PasswordEdit')
+    component (resolve) { require(['../views/user/PasswordEdit'], resolve) }
 }, { // 报名卡片  card
     path: '/user/card',
     name: '/user/card',
-    component: require('../views/user/EnrollCard')
+    component (resolve) { require(['../views/user/EnrollCard'], resolve) }
 }, { // 运动装备设置  card
     path: '/user/sportsDevice',
     name: '/user/sportsDevice',
-    component: require('../views/user/SportsDevice')
+    component (resolve) { require(['../views/user/SportsDevice'], resolve) }
 }, { // 编辑个人信息  userInfo
     path: '/user/infoEdit',
     name: '/user/infoEdit',
-    component: require('../views/user/UserInfoEdit')
+    component (resolve) { require(['../views/user/UserInfoEdit'], resolve) }
 }, { // 我的粉丝/关注的人  UserFans
     path: '/user/fans',
     name: '/user/fans',
-    component: require('../views/user/UserFans')
+    component (resolve) { require(['../views/user/UserFans'], resolve) }
 }, { // 查看用户（好友、粉丝、关注人）主页 UserInfoPage
     path: '/user/infoPage',
     name: '/user/infoPage',
-    component: require('../views/user/UserInfoPage')
+    component (resolve) { require(['../views/user/UserInfoPage'], resolve) }
 }, { // 俱乐部 Club
     path: '/club',
     name: '/club',
-    component: require('../views/Club')
+    component: require('../views/club')
 }, { // 俱乐部详情 ClubInfo
     path: '/club/info',
     name: '/club/info',
-    component: require('../views/club/ClubInfo')
+    component (resolve) { require(['../views/club/ClubInfo'], resolve) }
 }, { // 搜索模块 Search
     path: '/search',
     name: '/search',
-    component: require('../components/Search')
+    component (resolve) { require(['../components/Search'], resolve) }
 }, { // 路书 Line
     path: '/line',
     name: '/line',
-    component: require('../views/Line')
+    component: require('../views/line')
 }, { // 路书 LineInfo
     path: '/line/info',
     name: '/line/info',
-    component: require('../views/line/LineInfoPage')
+    component (resolve) { require(['../views/line/LineInfoPage'], resolve) }
 }, { // 地图 map
     path: '/map',
     name: '/map',
-    component: require('../views/Map')
+    component: require('../views/map')
 }]
 //  up更改合并
